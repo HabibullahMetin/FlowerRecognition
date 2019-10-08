3b8e1eb43a4deded99e79c0c160a5651ff7ba45a
HabibullahMetin <53422805+HabibullahMetin@users.noreply.github.com>
2019-10-08 22:49:00 +0300

    Add files via upload

---
+++ Ders_1/lesson_1.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+image = cv2.imread("seinfeldcast.jpg",0) 
+image2 = cv2.imread("newseinfeld.jpg")
+print(type(image))
+cv2.imshow("seinfeld image",image)
+cv2.imshow("seinfeldcast",image) 
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imwrite("newseinfeld.jpg",image)
+
+
+
+
+
